ef0e89414feab9518c04ab5ed337c267256c6f7d
Shreyash-ksolves <shreyash.pandey@ksolves.com>
2025-08-28 12:12:50 +0530

    Update AuthService.java

--- src/main/java/com/service/auth/services/AuthService.java
+++ src/main/java/com/service/auth/services/AuthService.java
@@ -3,24 +3,36 @@ package com.service.auth.services;
 
 import com.service.auth.entity.User;
 import com.service.auth.repository.UserRepository;
+import com.service.auth.security.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
-    @Autowired
-    UserRepository userRepository;
 
     @Autowired
-    PasswordEncoder passwordEncoder;
+    private UserRepository repository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-    public User registerUser(String username, String password, String role) {
-        if (userRepository.existsByUsername(username)) {
-            throw new RuntimeException("Username is already taken!");
-        }
+    @Autowired
+    private JwtUtil jwtUtil;
 
-        User user = new User(username, passwordEncoder.encode(password), role);
-        return userRepository.save(user);
+    public String saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        repository.save(user);
+        return "user added to the system";
     }
+
+    public String generateToken(Authentication authentication) {
+        return jwtUtil.generateToken(authentication);
+    }
+
+    public void validateToken(String token) {
+        jwtUtil.validateToken(token);
+    }
+
+
 }
